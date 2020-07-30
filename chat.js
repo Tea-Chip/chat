04957eba68f1f8571f9a4e6061c07a12ebf15a48
TomiPerfect <52126478+Tobiloba08@users.noreply.github.com>
2020-07-30 06:10:04 -0700

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,12 @@
+<span>Guest</span>Hello 
+<span>Guest</span>What's up 
+<span>Guest</span>akldj  
+<span>Guest</span>    
+<span>Guest</span>   
+<span>Guest</span>        
+<span>Kenrick</span>dwwd 
+<span>Guest</span>test 
+<span>Kenrick</span><a href="http://google.com" target="_blank">http://google.com</a> 
+<span>Guest</span><a href="http://css-tricks.com/examples/Chat/" target="_blank">http://css-tricks.com/examples/Chat/</a> 
+<span>Guest</span>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus 
+<span>Guest</span>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui.
